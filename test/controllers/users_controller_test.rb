147fda8d39182595628210e46bc2537968ad0ae4
otakekota@icloud.com <user@USERnoMacBook-Air.local>
2020-09-28 15:22:08 +0900

    layout and routes

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,10 +1,15 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
+
+  def setup
+    @base_title = "MoviePresentation"
+  end
+
   test "should get new" do
-    get users_new_url
+    get signup_path
     assert_response :success
-    assert_select "title", "New | MoviePresentation"
+    assert_select "title", "New | #{@base_title}"
   end
 
 end
