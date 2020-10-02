a867b6976f16174869929da5423f48f3019f25d8
otakekota@icloud.com <user@USERnoMacBook-Air.local>
2020-10-02 12:49:51 +0900

    basic login

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,4 +11,11 @@ class User < ApplicationRecord
   # has_sequre_password => password, password_confirmation, autthenticateメソッドが使える
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }
+
+
+  def User.digest(string)
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+                                                  BCrypt::Engine.cost
+    BCrypt::Password.create(string, cost: cost)
+  end
 end
--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -7,7 +7,6 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     assert_template 'static_pages/home'
     assert_select "a[href=?]", root_path, count: 3
     assert_select "a[href=?]", help_path
-    assert_select "a[href=?]", signup_path
     get help_path
     assert_select "title", full_title("Help")
   end
