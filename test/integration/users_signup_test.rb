f33789ecda6443cf97005bfccf69a0721532acf2
otakekota@icloud.com <user@USERnoMacBook-Air.local>
2020-10-03 11:26:40 +0900

    Add test login

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -4,6 +4,10 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
 
   test "invalid signup information" do
     get signup_path
+    assert_select 'input[type=hidden][name=?][value=?]',
+                                      "session[email]","example@example.com"
+    assert_select 'input[type=hidden][name=?][value=?]',
+                                      "session[password]","password"
     assert_no_difference 'User.count' do
       post users_path, params: { user: { name:  "",
                                          email: "user@invalid",
@@ -17,6 +21,10 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
 
   test "valid signup information" do
     get signup_path
+    assert_select 'input[type=hidden][name=?][value=?]',
+                                      "session[email]","example@example.com"
+    assert_select 'input[type=hidden][name=?][value=?]',
+                                      "session[password]","password"
     assert_difference 'User.count', 1 do
       post users_path, params: { user: { name:  "Example User",
                                          email: "user@example.com",
